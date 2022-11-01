13f1555967377e72fc752801b1ebc71f5591983d
maye156 <89624781+maye156@users.noreply.github.com>
2022-11-01 12:03:39 -0500

    actualización

---
+++ Semana 2-3/my-app-react/src/components/nuevaPag/nuevaPag.js
@@ -0,0 +1,12 @@
+import {Fragment} from 'react'
+
+//componente 
+export function Ap() {
+    return (
+        <Fragment>
+            <h1> Mi aplicación </h1>
+            <h2> es muy bonita </h2>
+        </Fragment>
+
+    );
+}
